ff293805dd1c2446673ef93720990a1526636034
Olivier <olivierdhxl@gmail.com>
2024-05-28 15:36:49 +0200

    ecriture de la classe Arc

--- src/Arc.java
+++ src/Arc.java
@@ -1,2 +1,21 @@
 public class Arc {
+    /**
+     * chaine correspondant au nom du noeud de destination de l'arc
+     */
+    private String dest;
+
+    /**
+     * attribut correspondant au cout de l'arc
+     */
+    private double cout;
+
+    /**
+     * constructeur d'Arc
+     * @param d est une chaine
+     * @param c est un reel
+     */
+    public void Arc(String d, double c) {
+        this.dest =  d;
+        this.cout = c;
+    }
 }
