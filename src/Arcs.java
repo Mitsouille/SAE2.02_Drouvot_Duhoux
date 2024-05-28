682650955f47e31195f702f6aca24836806eb050
Drouvot Melody <melody.drouvot@gmail.com>
2024-05-28 15:35:40 +0200

    Complétion Graphe et Arcs

--- src/Arcs.java
+++ src/Arcs.java
@@ -1,2 +1,17 @@
+import java.util.List;
+
 public class Arcs {
+    private List<Arc> arcs;
+
+    public Arcs(){
+        this.arcs = null;
+    }
+
+    public void ajouterArc(Arc a){
+        this.arcs.add(a);
+    }
+
+    public List<Arc> getArcs(){
+        return this.arcs;
+    }
 }
--- src/Graphe.java
+++ src/Graphe.java
@@ -1,2 +1,16 @@
-public class Graphe {
+import java.util.List;
+
+public interface Graphe {
+    /**
+     * Retourne tout les noeuds du graphe sous une forme de liste de String
+     * @return tout les noeuds du graphes
+     */
+    public List<String> listeNoeuds();
+
+    /**
+     * Retourne la liste des arcs du noeud passé
+     * @param n noeud passé en paramètre
+     * @return liste des arcs du noeuds
+     */
+    public List<Arc> suivants(String n);
 }
