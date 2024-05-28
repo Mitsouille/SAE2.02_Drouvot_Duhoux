e377e52d2f7719fa7ff3b438b2e74c327eeea8ce
Drouvot Melody <melody.drouvot@gmail.com>
2024-05-28 16:01:50 +0200

    Modification Main + Création test

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        GrapheListe graphe = new GrapheListe();
+        graphe.ajouterArc("A","D",87);
+        graphe.ajouterArc("A","B",12);
+        graphe.ajouterArc("B","E",11);
+        graphe.ajouterArc("C","A",19);
+        graphe.ajouterArc("D","B",23);
+        graphe.ajouterArc("D","C",10);
+        graphe.ajouterArc("E","D",43);
+
+    }
+}
---
+++ test/TestGraphe.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class TestGraphe {
+    @Test
+    public void test_GrapheExemple1(){
+
+        GrapheListe graphe = new GrapheListe();
+        graphe.ajouterArc("A","D",87);
+        graphe.ajouterArc("A","B",12);
+        graphe.ajouterArc("B","E",11);
+        graphe.ajouterArc("C","A",19);
+        graphe.ajouterArc("D","B",23);
+        graphe.ajouterArc("D","C",10);
+        graphe.ajouterArc("E","D",43);
+
+
+
+    }
+}
