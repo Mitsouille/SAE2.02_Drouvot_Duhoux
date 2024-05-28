93301d70dbc069f846cabcd7fc2d6dff65d6b91a
Drouvot Melody <melody.drouvot@gmail.com>
2024-05-28 16:30:14 +0200

    Modif TestGraph

--- test/TestGraphe.java
+++ test/TestGraphe.java
@@ -4,17 +4,9 @@ import java.util.ArrayList;
 
 public class TestGraphe {
     @Test
-    public void test_GrapheExemple1(){
-
-        GrapheListe graphe = new GrapheListe();
-        graphe.ajouterArc("A","D",87);
-        graphe.ajouterArc("A","B",12);
-        graphe.ajouterArc("B","E",11);
-        graphe.ajouterArc("C","A",19);
-        graphe.ajouterArc("D","B",23);
-        graphe.ajouterArc("D","C",10);
-        graphe.ajouterArc("E","D",43);
-
+    public void test_ConstructeurArc(){
+        Arc arc = new Arc("A",5.5);
+        AssertEquals("A",arc.);
 
 
     }
