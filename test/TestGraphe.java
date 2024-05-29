dfb97e151656743397af396f7dedb322e5ef7940
Drouvot Melody <melody.drouvot@gmail.com>
2024-05-29 10:10:38 +0200

    Ajout Exception + test

--- test/TestGraphe.java
+++ test/TestGraphe.java
@@ -35,11 +35,27 @@ public class TestGraphe {
         }catch(Exception e){
             exception = true;
         }
+        //Vérification
         assertEquals(true,exception,"Une erreur doit etre throw car un arc de cout négatif est impossible");
     }
 
     @Test
-    public void test_GrapheListe_ajout(){
+    public void test_GrapheAjoutNegatif(){
+        //Préparation des données
+        boolean exception = false;
+        GrapheListe graphe = new GrapheListe();
+        //Utilisation de la méthode
+        try{
+            graphe.ajouterArc("A","B",-5);
+        }catch (Exception e){
+            exception = true;
+        }
+        //Vérification
+        assertEquals(true,exception,"Une erreur doit etre throw car l'arc a un cout négatif");
+    }
+
+    @Test
+    public void test_GrapheListe_ajout() throws Exception{
         //Préparation des données
         GrapheListe graphe = new GrapheListe();
         //Utilisation de la méthode
@@ -48,5 +64,15 @@ public class TestGraphe {
         assertEquals("B",graphe.suivants("B").get(graphe.getIndice("B")).getDest());
         assertEquals(5.5,graphe.suivants("B").get(graphe.getIndice("B")).getCout());
     }
+
+    @Test
+    public void test_GrapheListe_SommetInexistant() throws Exception{
+        //Préparation des données
+        GrapheListe graphe = new GrapheListe();
+        //Vérification
+        assertEquals(-1,graphe.getIndice("A"),"-1 doit etre retourner car ce sommet n'est pas présent");
+    }
+
+
 
 }
