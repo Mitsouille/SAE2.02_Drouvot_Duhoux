e6c306001901dabdaf14851467f9bad8e32f9261
Drouvot Melody <melody.drouvot@gmail.com>
2024-05-31 10:37:49 +0200

    Changement MainPerformance

--- src/MainCalculPerformance.java
+++ src/MainCalculPerformance.java
@@ -23,7 +23,7 @@ public class MainCalculPerformance {
         long date_fin = System.nanoTime();
         //Calcul de la différence
         long tempsFinal = date_fin - date_debut;
-        System.out.println("Temps nécéssaire pour 2 répétitions pour résoudre tout les graphes \n avec algo de Dijkstra : " + tempsFinal + " en nanosecondes");
+        System.out.println("Temps nécéssaire pour résoudre tout les graphes \n avec algo de Dijkstra : " + tempsFinal + " en nanosecondes");
         System.out.println("Ce qui représente : " + tempsFinal * 1E-9 + " secondes");
         //Début du chrono
         long date_debut2 = System.nanoTime();
@@ -36,7 +36,7 @@ public class MainCalculPerformance {
         long date_fin2 = System.nanoTime();
         //Calcul de la différence
         long tempsFinal2 = date_fin2 - date_debut2;
-        System.out.println("Temps nécéssaire pour 2 répétitions pour résoudre tout les graphes \n avec algo de BellmanFord : " + tempsFinal2 + " en nanosecondes");
+        System.out.println("Temps nécéssaire pour résoudre tout les graphes \n avec algo de BellmanFord : " + tempsFinal2 + " en nanosecondes");
         System.out.println("Ce qui représente : " + tempsFinal2 * 1E-9 + " secondes");
 
 
